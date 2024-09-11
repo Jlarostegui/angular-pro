201eb6c517fa2c1ed0ae45d7a1f1e1cf7da8af23
Luis <Luis.garcia.arostegui@gmail.com>
2024-09-12 00:10:23 +0200

    Separacion componenrtes

--- 01-zoneless-calculator/src/app/calculator/views/calculator_view/calculator_view.component.ts
+++ 01-zoneless-calculator/src/app/calculator/views/calculator_view/calculator_view.component.ts
@@ -1,10 +1,11 @@
-import { CommonModule } from '@angular/common';
+import { CalculatorComponent } from '@/calculator/components/calculator/calculator.component';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'calculator-view',
   standalone: true,
   imports: [
+    CalculatorComponent
   ],
   templateUrl: './calculator_view.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
