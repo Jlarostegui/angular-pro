35d1d4c20169659ec44cb42836b99f8c4f3278b1
Luis <Luis.garcia.arostegui@gmail.com>
2024-09-11 23:59:50 +0200

    Diseño y estructura

--- 01-zoneless-calculator/src/app/app.routes.ts
+++ 01-zoneless-calculator/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'calculator',
+        loadComponent: () => import('@/calculator/views/calculator_view/calculator_view.component'),
+    },
+    {
+        path: '**',
+        redirectTo: 'calculator'
+    }
+];
